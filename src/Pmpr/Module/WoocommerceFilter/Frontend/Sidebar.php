0b2582e1d5335145b210a8df7d89f6fd31093e2b
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 20:07:36 +0000

    Obfuscated

--- src/Pmpr/Module/WoocommerceFilter/Frontend/Sidebar.php
+++ src/Pmpr/Module/WoocommerceFilter/Frontend/Sidebar.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66e5e7d895647             |
+    |             66e5ece1a51a2             |
     |_______________________________________|
 */
- namespace Pmpr\Module\WoocommerceFilter\Frontend; class Sidebar extends Common { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\167\157\x6f\143\157\155\155\x65\162\x63\145\x5f\x73\151\x64\x65\x62\x61\162", [$this, "\x72\x65\x6e\144\x65\162"], 50); } public function render() { if (!($this->wwqoiqcoccacyyyc() && $this->caokeucsksukesyo()->aqasygcsqysmmyke()->omuogooguicuqewu())) { goto mwysseaekcsiesmm; } $this->skqqcuwuuumqkykk(); mwysseaekcsiesmm: } }
+ namespace Pmpr\Module\WoocommerceFilter\Frontend; class Sidebar extends Common { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\x77\157\x6f\x63\157\155\x6d\x65\x72\x63\x65\137\x73\x69\144\145\x62\141\x72", [$this, "\162\x65\156\x64\145\x72"], 50); } public function render() { if (!($this->wwqoiqcoccacyyyc() && $this->caokeucsksukesyo()->aqasygcsqysmmyke()->omuogooguicuqewu())) { goto mwysseaekcsiesmm; } $this->skqqcuwuuumqkykk(); mwysseaekcsiesmm: } }
--- src/Pmpr/Module/WoocommerceFilter/Model/Model.php
+++ src/Pmpr/Module/WoocommerceFilter/Model/Model.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66e5e7d895647             |
+    |             66e5ece1a51a2             |
     |_______________________________________|
 */
  namespace Pmpr\Module\WoocommerceFilter\Model; use Pmpr\Module\WoocommerceFilter\Container; class Model extends Container { public function mameiwsayuyquoeq() { Cache::symcgieuakksimmu(); } }
