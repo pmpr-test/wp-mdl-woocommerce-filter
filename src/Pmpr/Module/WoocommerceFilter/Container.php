993e42cba2150737ceda9557ab6981744dff42ab
pmpr.dev <pmpr.dev@gmail.com>
2024-12-28 20:22:20 +0000

    Obfuscated

--- src/Pmpr/Module/WoocommerceFilter/Container.php
+++ src/Pmpr/Module/WoocommerceFilter/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             673bbd8ed60d7             |
+    |             67705dd05bf55             |
     |_______________________________________|
 */
  namespace Pmpr\Module\WoocommerceFilter; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\WoocommerceFilter\Traits\CommonTrait; abstract class Container extends BaseClass { use CommonTrait; }
--- src/Pmpr/Module/WoocommerceFilter/Traits/CommonTrait.php
+++ src/Pmpr/Module/WoocommerceFilter/Traits/CommonTrait.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             673bbd8ed60d7             |
+    |             67705dd05bf55             |
     |_______________________________________|
 */
- namespace Pmpr\Module\WoocommerceFilter\Traits; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Module\WoocommerceFilter\Helper; use Pmpr\Module\WoocommerceFilter\Setting; use Pmpr\Module\WoocommerceFilter\WoocommerceFilter; use WP_Taxonomy; trait CommonTrait { public function kokwyquiqyoaaioc() : Helper { return Helper::symcgieuakksimmu(); } public function qeomwskywaguqyyu() : bool { return Setting::symcgieuakksimmu()->giiuwsmyumqwwiyq(Setting::ykqqgqskiygugscu, false); } public function mqmocoguqcyymgqu() : ?array { return $this->caokeucsksukesyo()->aqasygcsqysmmyke()->mqmocoguqcyymgqu(); } public function wwqoiqcoccacyyyc() : bool { return $this->ocksiywmkyaqseou(WoocommerceFilter::geiygweugseyomyy . "\x61\154\154\157\x77\x5f\141\144\x64", $this->caokeucsksukesyo()->aqasygcsqysmmyke()->omuogooguicuqewu()); } }
+ namespace Pmpr\Module\WoocommerceFilter\Traits; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Module\WoocommerceFilter\Helper; use Pmpr\Module\WoocommerceFilter\Setting; use Pmpr\Module\WoocommerceFilter\WoocommerceFilter; use WP_Taxonomy; trait CommonTrait { public function kokwyquiqyoaaioc() : Helper { return Helper::symcgieuakksimmu(); } public function qeomwskywaguqyyu() : bool { return Setting::symcgieuakksimmu()->giiuwsmyumqwwiyq(Setting::ykqqgqskiygugscu, false); } public function mqmocoguqcyymgqu() : ?array { return $this->caokeucsksukesyo()->aqasygcsqysmmyke()->mqmocoguqcyymgqu(); } public function wwqoiqcoccacyyyc() : bool { return $this->ocksiywmkyaqseou(WoocommerceFilter::geiygweugseyomyy . "\x61\x6c\x6c\x6f\x77\x5f\141\x64\144", $this->caokeucsksukesyo()->aqasygcsqysmmyke()->omuogooguicuqewu()); } }
